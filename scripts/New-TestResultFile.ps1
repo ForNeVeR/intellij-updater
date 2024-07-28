d9ba9adf7828b16c9018b8fc35e0635ccba8e876
Friedrich von Never <friedrich@fornever.me>
2024-07-29 00:13:12 +0200

    CI: add the test result preparation script

---
+++ scripts/New-TestResultFile.ps1
@@ -0,0 +1,34 @@
+﻿# SPDX-FileCopyrightText: 2024 Friedrich von Never <friedrich@fornever.me>
+#
+# SPDX-License-Identifier: MIT
+
+param (
+    [Parameter(Mandatory = $true)]
+    [string] $HasChanges,
+    [Parameter(Mandatory = $true)]
+    [string] $BranchName,
+    [Parameter(Mandatory = $true)]
+    [string] $AuthorName,
+    [Parameter(Mandatory = $true)]
+    [string] $CommitMessage,
+    [Parameter(Mandatory = $true)]
+    [string] $PrTitle,
+    [Parameter(Mandatory = $true)]
+    [string] $PrBodyPath,
+    [Parameter(Mandatory = $true)]
+    [string] $OutFile
+)
+
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version Latest
+
+$prBody = Get-Content -LiteralPath $PrBodyPath
+
+@"
+has-changes=$HasChanges
+branch-name=$BranchName
+author-name=$AuthorName
+commit-message=$CommitMessage
+pr-title=$PrTitle
+pr-body=$PrBody
+"@ | Out-File -LiteralPath $OutFile
