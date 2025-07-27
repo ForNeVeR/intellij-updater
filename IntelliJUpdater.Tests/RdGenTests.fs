1e8331fd2e0c016842ac54013aba35179c50ce12
Friedrich von Never <friedrich@fornever.me>
2025-07-27 22:26:15 +0200

    RdGenTests: accommodate for rd-gen 2024.3.2

--- IntelliJUpdater.Tests/RdGenTests.fs
+++ IntelliJUpdater.Tests/RdGenTests.fs
@@ -14,6 +14,6 @@ open Xunit
 let ``RdGen version determiner``(): Task = task {
     let! v243 = RdGen.ForIde <| YearBased(2024, 3)
     let! v251 = RdGen.ForIde <| YearBasedVersion 251
-    Assert.Equal(Version.Parse("2024.3.1"), v243)
+    Assert.Equal(Version.Parse("2024.3.2"), v243)
     Assert.Equal(Version.Parse("2025.1.1"), v251)
 }
